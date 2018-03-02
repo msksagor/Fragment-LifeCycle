7878fcf909be1becdb7e806cda1aa859e1d21947
sagor <msksagor@gmail.com>
2018-03-02 16:40:42 +0600

    Fragment to Fragment Data pass by Argument Bundles

--- app/src/main/java/com/example/fondn/sabujfragment/MainActivity.java
+++ app/src/main/java/com/example/fondn/sabujfragment/MainActivity.java
@@ -8,35 +8,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    private Button one, two;
+    private Button one, two,three,four;
     FragmentManager fragmentManager;
     BlankFragment blankFragment;
     FillFragment fillFragment;
+    SenderFragment senderFragment;
+    ReceiverFragment receiverFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         blankFragment = new BlankFragment();
+        blankFragment = new BlankFragment();
          fillFragment = new FillFragment();
+         senderFragment = new SenderFragment();
+         receiverFragment = new ReceiverFragment();
+
         one = (Button) findViewById(R.id.oneButtonID);
         two = (Button) findViewById(R.id.twoButtonID);
+        three = (Button) findViewById(R.id.threeButtonID);
+        four = (Button) findViewById(R.id.fourButtonID);
+        fragmentManager = getFragmentManager();
 
-        one.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "One", Toast.LENGTH_SHORT).show();
 
-                fragmentManager = getFragmentManager();
-                FragmentTransaction ft = fragmentManager.beginTransaction();
-                ft.replace(R.id.fragmentContainerID, blankFragment);
-                ft.addToBackStack(null);
-                ft.commit();
-            }
-        });
 
         two.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +46,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Sender Fragment", Toast.LENGTH_SHORT).show();
+                fragmentManager = getFragmentManager();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.replace(R.id.fragmentContainerID, senderFragment,"s");
+                ft.commit();
+            }
+        });
+
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Receiver Fragment", Toast.LENGTH_SHORT).show();
+                fragmentManager = getFragmentManager();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.replace(R.id.fragmentContainerID, receiverFragment,"r");
+                ft.commit();
+            }
+        });
+
 
     }
+
+
+
 
 }
